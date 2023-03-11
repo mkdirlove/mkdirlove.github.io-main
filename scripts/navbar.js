05c1d4bc8f6bde404eec67b59f091e406ea6e71c
Jayson Cabrillas San Buenaventura <52001009+mkdirlove@users.noreply.github.com>
2023-03-11 20:19:28 +0800

    Update navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -42,10 +42,10 @@ const updateNavbarContent = () => {
     if (hashAndLinkedLinks[window.location.hash]) {
         if(window.location.hash !== '#home') hashAndLinkedLinks[window.location.hash].classList.add('active-link')
         document.getElementById('current-location').innerText = window.location.hash.substring(1)
-        document.title = `zave | ${window.location.hash.substring(1)}`
+        document.title = `mkdirlove | ${window.location.hash.substring(1)}`
     } else {
         document.getElementById('current-location').innerText = '404'
-        document.title = `zave | ${404}`
+        document.title = `mkdirlove | ${404}`
     }
     
     typeNavbar()
